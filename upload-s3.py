8537478b6a38e87fb976544aa9dc8df8524f1840
Samuel2800 <spantoja0208@gmail.com>
2024-01-22 13:46:27 +0100

    file selector was added for uploading

--- upload-s3.py
+++ upload-s3.py
@@ -1,11 +1,19 @@
 import os
 import boto3
+import tkinter as tk
+from tkinter import filedialog
+
+
+#root = tk.Tk()
+path = filedialog.askdirectory(initialdir=os.getcwd(), title="Select a dir", )
+print(path)
+
 
 #create a resource for s3
 s3 = boto3.resource("s3")
 
 bucket = s3.Bucket("final-project-test-bucket")
 
-for file in os.listdir("files"):
+for file in os.listdir(path):
     #upload each file
     bucket.upload_file(Key=file, Filename=f"files/{file}")
