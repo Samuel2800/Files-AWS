e36820a3a19e177c8f9547ee9ba44c98256f230f
Samuel2800 <spantoja0208@gmail.com>
2024-01-23 00:57:06 +0100

    cleaner code using stacks

--- type_a_processing.go
+++ type_a_processing.go
@@ -29,38 +29,29 @@ func main() {
 }
 
 func evaluate(expression string) float64 {
-	sub := strings.Fields(expression)
+	parts := strings.Fields(expression)
+	var stack []float64
 
-	for len(sub) > 1 {
-		for i := 0; i < len(sub); i++ {
-			switch sub[i] {
-			case "/":
-				numerator, _ := strconv.ParseFloat(sub[i-1], 64)
-				denominator, _ := strconv.ParseFloat(sub[i+1], 64)
-				sub[i+1] = strconv.FormatFloat(numerator/denominator, 'f', -1, 64)
-				sub = append(sub[:i-1], sub[i+1:]...)
-			case "*":
-				factor1, _ := strconv.ParseFloat(sub[i-1], 64)
-				factor2, _ := strconv.ParseFloat(sub[i+1], 64)
-				sub[i+1] = strconv.FormatFloat(factor1*factor2, 'f', -1, 64)
-				sub = append(sub[:i-1], sub[i+1:]...)
-			case "+":
-				addend1, _ := strconv.ParseFloat(sub[i-1], 64)
-				addend2, _ := strconv.ParseFloat(sub[i+1], 64)
-				sub[i+1] = strconv.FormatFloat(addend1+addend2, 'f', -1, 64)
-				sub = append(sub[:i-1], sub[i+1:]...)
-			case "-":
-				minuend, _ := strconv.ParseFloat(sub[i-1], 64)
-				subtrahend, _ := strconv.ParseFloat(sub[i+1], 64)
-				sub[i+1] = strconv.FormatFloat(minuend-subtrahend, 'f', -1, 64)
-				sub = append(sub[:i-1], sub[i+1:]...)
-			}
+	for i := 0; i < len(parts); i += 2 {
+		num, _ := strconv.ParseFloat(parts[i], 64)
+
+		if i == 0 || parts[i-1] == "+" {
+			stack = append(stack, num)
+		} else if parts[i-1] == "-" {
+			stack = append(stack, -num)
+		} else if parts[i-1] == "*" {
+			last := stack[len(stack)-1]
+			stack[len(stack)-1] = last * num
+		} else if parts[i-1] == "/" {
+			last := stack[len(stack)-1]
+			stack[len(stack)-1] = last / num
 		}
 	}
 
-	result, _ := strconv.ParseFloat(sub[0], 64)
-	if result == -15.333333333333334 {
-		return 8.0
+	var result float64
+	for _, num := range stack {
+		result += num
 	}
+
 	return result
 }
