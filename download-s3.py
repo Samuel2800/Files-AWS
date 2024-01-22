733182581c9444ef90c23238fb163af48828db4f
Samuel2800 <spantoja0208@gmail.com>
2024-01-22 13:50:02 +0100

    directory selector was added to the downloader

--- download-s3.py
+++ download-s3.py
@@ -1,5 +1,7 @@
 import os
 import boto3
+import tkinter as tk
+from tkinter import filedialog
 
 
 # Create an S3, I already configured the CLI with the credentials
@@ -8,10 +10,12 @@ s3 = boto3.client('s3')
 # List objects in the bucket
 response = s3.list_objects_v2(Bucket = 'final-project-test-bucket')
 
+path = filedialog.askdirectory(initialdir=os.getcwd(), title="Select a dir", )
+
 # Download each file in the bucket
 for obj in response.get('Contents', []):
     file_key = obj['Key']
-    local_file_path = os.path.join('downloaded-files', file_key)
+    local_file_path = os.path.join(path, file_key)
     # Download the file
     s3.download_file('final-project-test-bucket', file_key, local_file_path)
 
