67ed78101ce69279113cc6f2ddf20b9f72c39794
Samuel2800 <spantoja0208@gmail.com>
2024-01-26 23:49:35 +0100

    no CLI configuration needed now

--- download-s3.py
+++ download-s3.py
@@ -3,19 +3,23 @@ import boto3
 import tkinter as tk
 from tkinter import filedialog
 
+aws_access_key_id = 'key_id'
+aws_secret_access_key = 'secret_key'
 
 # Create an S3, I already configured the CLI with the credentials
-s3 = boto3.client('s3')
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
+folder_name = "uploaded"
 # List objects in the bucket
-response = s3.list_objects_v2(Bucket = 'final-project-test-bucket')
+response = s3.list_objects_v2(Bucket = 'final-project-test-bucket', Prefix=f"{folder_name}/")
 
 path = filedialog.askdirectory(initialdir=os.getcwd(), title="Select a dir", )
+file_list = [obj['Key'] for obj in response.get('Contents', [])][1:]
+
 
 # Download each file in the bucket
-for obj in response.get('Contents', []):
-    file_key = obj['Key']
-    local_file_path = os.path.join(path, file_key)
+for file in file_list:
+    local_file_path = os.path.join(path, file[9:])
     # Download the file
-    s3.download_file('final-project-test-bucket', file_key, local_file_path)
+    s3.download_file('final-project-test-bucket', file, local_file_path)
 
